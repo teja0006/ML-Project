c86106199b986467d15ec832917854bb48413fa2
DATTA TEJA <dattateja1234@gmail.com>
2024-09-22 20:11:57 +0530

    Model Training

--- src/components/utils.py
+++ src/components/utils.py
@@ -5,6 +5,21 @@ from src.exception import CustomException
 import numpy as np
 import pandas as pd
 
+from catboost import CatBoostRegressor
+from sklearn.ensemble import(
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor
+)
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LinearRegression
+from xgboost import XGBRegressor
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
+from sklearn.metrics import r2_score
+
+
 import dill
 
 def save_object(file_path,obj):
@@ -15,6 +30,29 @@ def save_object(file_path,obj):
 
         with open(file_path,'wb') as file_obj:
             dill.dump(obj,file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(X_train,y_train)
+
+            y_pred_train = model.predict(X_train)
+            y_pred_test = model.predict(X_test)
+
+            train_model_score = r2_score(y_train,y_pred_train)
+            test_model_score = r2_score(y_test,y_pred_test)
+
+            report[list(models.keys())[i]] = test_model_score
+        
+        return report
+
 
     except Exception as e:
         raise CustomException(e,sys)
