23659ceffdf0083dcf8d9b1631802716c56eece3
DATTA TEJA <dattateja1234@gmail.com>
2024-09-25 18:59:34 +0530

    Hyperparameter Tuning

--- src/components/utils.py
+++ src/components/utils.py
@@ -13,12 +13,11 @@ from sklearn.ensemble import(
 )
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 from xgboost import XGBRegressor
-from sklearn.linear_model import Lasso
-from sklearn.linear_model import Ridge
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 
 import dill
 
@@ -34,13 +33,18 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=params[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             y_pred_train = model.predict(X_train)
